2635b0a97ff9670586d8a59ef5523b04b213c562
Camilo Cunha de Azevedo <cazevedo@localhost.localdomain>
2019-05-05 23:53:47 -0300

    Devia ter ido!

--- controle_gastos/urls.py
+++ controle_gastos/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from contas.views import home, nova_transacao, update_transacao
+from contas.views import home, nova_transacao, update_transacao, delete_transacao
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='url_home'),
     path('nova/', nova_transacao, name='url_nova_transacao'),
     path('editar/<int:pk>/', update_transacao, name='url_update'),
+    path('deletar/<int:pk>/', delete_transacao, name='url_delete'),
 ]
